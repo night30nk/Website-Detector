8a9a2b55864427f671c8447748b806e164f0ab9c
niGht30nk <nisha257372@gmail.com>
2025-07-10 10:13:24 +0530

    2-07-2025

--- app.py
+++ app.py
@@ -59,5 +59,8 @@ def home():
 
     return render_template('index.html', prediction=prediction, url_input=url_input)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
